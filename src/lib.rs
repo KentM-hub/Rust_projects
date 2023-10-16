9d55c8198bbb8def499e41d163e21974302ea3fe
KentM-hub <kent20020324@gmail.com>
2023-10-16 17:25:06 +0900

    feat:41pまで完了

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 use wasm_bindgen::JsCast;
 use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
 //use async_std::task;
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -80,14 +82,27 @@ pub fn main_js() -> Result<(), JsValue> {
             .unwrap();
 
             wasm_bindgen_futures::spawn_local(async move {
-                let (success_tx,success_rx) = futures::channel::oneshot::channel::<>();
-            
+                let (success_tx,success_rx) = futures::channel::oneshot::channel::<Result<(),JsValue>>();
+                
+                let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+                let error_tx = Rc::clone(&success_tx);
                 let image  = web_sys::HtmlImageElement::new().unwrap();
                 let callback = Closure::once(move || {
-                    success_tx.send(());
-                    web_sys::console::log_1(&JsValue::from_str("loaded"));
+                    if let Some(success_tx) =success_tx.lock().ok().and_then(|mut opt| opt.take()){
+                        success_tx.send(Ok(()));
+                    }
+                        
+                        //.send(Ok(()));
+                    //web_sys::console::log_1(&JsValue::from_str("loaded"));
+                });
+                let error_callback = Closure::once(move |err| {
+                    if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()){
+                        error_tx.send(Err(err));
+                    }
+                        
                 });
                 image.set_onload(Some(callback.as_ref().unchecked_ref()));
+                image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
                 callback.forget();
                 image.set_src("Idle (1).png");
                 success_rx.await;
