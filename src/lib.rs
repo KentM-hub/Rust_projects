91d6905a376ccd92ba9db64d302b26baa15055a2
KentM-hub <kent20020324@gmail.com>
2023-10-18 17:47:30 +0900

    p48まで完了

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use wasm_bindgen::JsCast;
 use rand::prelude::*;
 use std::rc::Rc;
 use std::sync::Mutex;
+
+use serde::Deserialize;
+use std::collections::HashMap;
 //use async_std::task;
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -13,7 +16,22 @@ use std::sync::Mutex;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String,Cell>,
+}
 
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
 
 fn draw_triangle(  
     context: &web_sys::CanvasRenderingContext2d, 
@@ -113,9 +131,91 @@ pub fn main_js() -> Result<(), JsValue> {
                     (0,255,0),
                     5
                 );
+
+                context.draw_image_with_html_image_element(&image,0.0,0.0);
+                let json = fetch_json("rhb.json")
+                    .await
+                    .expect("Could not fetch rhb.json");
+
+                let sheet: Sheet= json
+                    .into_serde()
+                    .expect("Could not convert rhb.json into a Sheet structure");
+
+                let (success_tx,success_rx) = futures::channel::oneshot::channel::<Result<(),JsValue>>();
+                
+                let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+                let error_tx = Rc::clone(&success_tx);
+                let image  = web_sys::HtmlImageElement::new().unwrap();
+                let callback = Closure::once(move || {
+                    if let Some(success_tx) =success_tx.lock().ok().and_then(|mut opt| opt.take()){
+                        success_tx.send(Ok(()));
+                    }
+                        
+                        //.send(Ok(()));
+                    //web_sys::console::log_1(&JsValue::from_str("loaded"));
+                });
+                let error_callback = Closure::once(move |err| {
+                    if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()){
+                        error_tx.send(Err(err));
+                    }
+                        
+                });
+                image.set_onload(Some(callback.as_ref().unchecked_ref()));
+                image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+                callback.forget();
+                image.set_src("rhb.png");
+                success_rx.await;
+
+                let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
+                context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                    &image,
+                    sprite.frame.x.into(),
+                    sprite.frame.y.into(),
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                    300.0,
+                    300.0,
+                    sprite.frame.w.into(),
+                    sprite.frame.h.into(),
+                );
+
             
             
         });
     
     Ok(())
 }
+
+async fn fetch_json(json_path: &str) -> Result<JsValue,JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(
+        window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
