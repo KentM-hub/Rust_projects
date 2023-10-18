e4e59384aa29823ec66913d8c9e5b0a5e0090b29
KentM-hub <kent20020324@gmail.com>
2023-10-18 17:54:08 +0900

    p49まで完了

--- src/lib.rs
+++ src/lib.rs
@@ -33,53 +33,6 @@ struct Cell {
     frame: Rect,
 }
 
-fn draw_triangle(  
-    context: &web_sys::CanvasRenderingContext2d, 
-    points: [(f64,f64); 3],
-    color:(u8,u8,u8),
-    ) {
-    let color_str= format!("rgb({},{},{})",color.0,color.1,color.2);
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    let [top,left,right] = points;
-    context.move_to(top.0,top.1);
-    context.begin_path();
-    context.line_to(left.0,left.1);
-    context.line_to(right.0,right.1);  
-    context.line_to(top.0,top.1);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-fn midpoint(point_1:(f64,f64),point_2:(f64,f64)) ->(f64,f64){
-    ((point_1.0+point_2.0)/2.0,(point_1.1+point_2.1)/2.0)
-}
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d, 
-    points: [(f64,f64); 3],
-    color: (u8,u8,u8),
-    depth: u8,
-    ) {
-    let [top,left,right] = points;
-    draw_triangle(&context,points,color);
-    let depth=depth-1;
-    if depth>0 {
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-        let left_middle = midpoint(top,left);
-        let right_middle = midpoint(top,right);
-        let bottom_middle = midpoint(left,right);
-        sierpinski(&context, [top,left_middle,right_middle], next_color, depth);
-        sierpinski(&context, [left_middle,left,bottom_middle], next_color, depth);
-        sierpinski(&context, [right_middle,bottom_middle,right], next_color, depth);
-    }
-
-}
-
-
 
 
 
@@ -100,39 +53,7 @@ pub fn main_js() -> Result<(), JsValue> {
             .unwrap();
 
             wasm_bindgen_futures::spawn_local(async move {
-                let (success_tx,success_rx) = futures::channel::oneshot::channel::<Result<(),JsValue>>();
-                
-                let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-                let error_tx = Rc::clone(&success_tx);
-                let image  = web_sys::HtmlImageElement::new().unwrap();
-                let callback = Closure::once(move || {
-                    if let Some(success_tx) =success_tx.lock().ok().and_then(|mut opt| opt.take()){
-                        success_tx.send(Ok(()));
-                    }
-                        
-                        //.send(Ok(()));
-                    //web_sys::console::log_1(&JsValue::from_str("loaded"));
-                });
-                let error_callback = Closure::once(move |err| {
-                    if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()){
-                        error_tx.send(Err(err));
-                    }
-                        
-                });
-                image.set_onload(Some(callback.as_ref().unchecked_ref()));
-                image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-                callback.forget();
-                image.set_src("Idle (1).png");
-                success_rx.await;
-                context.draw_image_with_html_image_element(&image,0.0,0.0);
-                sierpinski(
-                    &context, 
-                    [(300.0,0.0),(0.0,600.0),(600.0,600.0)],
-                    (0,255,0),
-                    5
-                );
-
-                context.draw_image_with_html_image_element(&image,0.0,0.0);
+               
                 let json = fetch_json("rhb.json")
                     .await
                     .expect("Could not fetch rhb.json");
